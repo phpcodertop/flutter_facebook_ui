4303a01e3e9780b98358efc80b911def3bc8098f
Ahmed Maher Halima <phpcodertop@gmail.com>
2024-07-02 07:01:16 +0300

    add assets files

---
+++ lib/models/post.dart
@@ -0,0 +1,26 @@
+class Comment {
+  final String content;
+  final String author;
+  final String createdAt;
+
+  const Comment({ required this.author, required this.content, required this.createdAt,});
+}
+
+class Post {
+  final String content;
+  final String author;
+  final String authorImg;
+  final String createdAt;
+  final bool isLiked;
+  List<Comment>? comments;
+  String? imagePath;
+
+  Post({
+    required this.content,
+    required this.author,
+    required this.authorImg,
+    required this.createdAt,
+    this.isLiked = false,
+    this.imagePath,
+  });
+}
